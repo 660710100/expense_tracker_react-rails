a93b4a9aeae0998f261f9dc7566fa8ec0217ab10
660710100 <chaiyapat_s@silpakorn.edu>
2026-03-13 12:51:05 +0700

    add route for rails

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "expenses#index"
+  
   resources :expenses, only: [:index, :create, :destroy]
 end
